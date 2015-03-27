59e10fcc4fd6256ea7bf9655a22eaef66deecca8
Luke Zhang <zhangl@rose-hulman.edu>
2015-03-27 18:42:48 -0400

    Modify PrimeFactos
    Passed all 9 unit tests now.

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class PrimeFactors {
 
 	public static List<Integer> generate(int i) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		for (;i%2 == 0; i/= 2 )
-		{
-			primes.add(2);
+		int canadidate = 2;
+		while (i > 1) {
+			for (; i % canadidate == 0; i /= 2) {
+				primes.add(canadidate);
+			}
+			canadidate++;
 		}
-		if (i>1)
-		{
+		if (i > 1) {
 			primes.add(i);
 		}
 		return primes;
