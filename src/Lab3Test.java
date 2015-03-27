a7e297e402eb4ace3d7f0fdf59793ab2a00f14db
Luke Zhang <zhangl@rose-hulman.edu>
2015-03-27 17:26:49 -0400

    Create First Test

--- src/Lab3Test.java
+++ src/Lab3Test.java
@@ -1,12 +1,18 @@
 import static org.junit.Assert.*;
-
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
+
 
 
 public class Lab3Test {
 
 	@Test
 	public void testOne() {
-        assertEquals(list(), PrimeFactos.generate(1));
+        assertEquals(list(), PrimeFactors.generate(1));
+	}
+
+	private  List<Integer> list() {
+		return Arrays.asList();
 	}
 }
---
+++ src/PrimeFactors.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+
+public class PrimeFactors {
+
+	public static List<Integer> generate(int i) {
+		return null;
+	}
+
+}
