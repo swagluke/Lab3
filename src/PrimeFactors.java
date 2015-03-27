428a5bf539d6e79da09f828e3e57d65c88450d4e
Luke Zhang <zhangl@rose-hulman.edu>
2015-03-27 18:30:53 -0400

    Modify PrimeFactors
    Add a new if statement. So 4 tests are passed.

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -6,6 +6,11 @@ public class PrimeFactors {
 
 	public static List<Integer> generate(int i) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
+		if (i%2 == 0 )
+		{
+			primes.add(2);
+			i /=2;
+		}
 		if (i>1)
 		{
 			primes.add(i);
