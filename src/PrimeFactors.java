cd05dfa33deeb556c4682a31170028047e23ca5c
Luke Zhang <zhangl@rose-hulman.edu>
2015-03-27 18:46:02 -0400

    Modify PrimeFactors.

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -5,15 +5,10 @@ public class PrimeFactors {
 
 	public static List<Integer> generate(int i) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		int canadidate = 2;
-		while (i > 1) {
-			for (; i % canadidate == 0; i /= 2) {
+		for (int canadidate = 2;i > 1;canadidate++) {
+			for (; i % canadidate == 0; i /= canadidate) {
 				primes.add(canadidate);
 			}
-			canadidate++;
-		}
-		if (i > 1) {
-			primes.add(i);
 		}
 		return primes;
 	}
