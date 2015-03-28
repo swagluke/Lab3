3e8f949926b40374343a21e3dd5030e16d39bc11
Luke Zhang <zhangl@rose-hulman.edu>
2015-03-27 20:15:23 -0400

    Create First Test
    Also Modify code after creating the test

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 
 public class PrimeGenerator {
+
+	public static ArrayList<Integer> generator(int testGiven) {
+		return null;
+	}
+
+
 
 }
--- src/PrimeGeneratorTest.java
+++ src/PrimeGeneratorTest.java
@@ -1,13 +1,29 @@
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-
+@RunWith(Parameterized.class)
 public class PrimeGeneratorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	int testGiven;
+	ArrayList<Integer> testReturned;
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> primeNumbers() {
+		Integer[] first = {};
+		return Arrays.asList(new Object [] [] {{1, new ArrayList<Integer>(Arrays.asList(first))},});
 	}
 
+	public PrimeGeneratorTest(int given, ArrayList<Integer> returned) {
+		testGiven = given;
+		testReturned = returned;
+	}
+	@Test
+	public void realTest() {
+		assertEquals(testReturned, PrimeGenerator.generator(testGiven));
+	}
 }
